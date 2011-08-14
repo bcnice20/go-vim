242e9a58159ac6d58d0307bf69df81a106ae18b7
Brad Carter <bcnice20@gmail.com>
2011-08-14 18:03:39 -0400

    updated readme to reflect proper additions to .vimrc and made install script slightly less error prone.

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
-#!/bin/bash 
+#!/bin/bash -
 
 #-------------------------------------------------------------------------#
 #                                                                         #
 #                       Full Go support for Vim                           #
 #                       packaged by:Brad Carter                           #
-#                       version:1.0                                       #
+#                       version:1.01                                      #
 #                                                                         #
 #-------------------------------------------------------------------------#
 
-dir=~/vim-go-support
+
 vim=~/.vim
 
-cd $dir
+mkdir -p $vim
+mkdir -p $vim/{autoload,ftdetect,ftplugin,indent,plugin,syntax}
+
+
 cp -r * $vim
 cd $vim
 rm install.sh
+rm README.md
+
 echo "go support is now installed at $vim"
 
